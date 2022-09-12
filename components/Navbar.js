db059928b7d016e98707c81c8312956c6d82e268
YAKSHIT-22 <yakshitgarg25@gmail.com>
2022-09-12 15:23:45 +0530

    Added Footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ const NavBar = ({cartQty}) => {
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link href='/'>
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">  
-                <img src="/logo-landscape.png"/>
+                <img src="/Group.svg"/>
             </a>
           </Link> 
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">

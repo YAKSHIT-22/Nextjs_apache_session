82756c571e39c9db74c1611a2e0f067d5dc49867
YAKSHIT-22 <yakshitgarg25@gmail.com>
2022-09-13 12:13:08 +0530

    Added Products

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,11 +4,11 @@ import Link from 'next/link'
 const NavBar = ({cartQty}) => {
   return (
     <div>
-        <header className="text-gray-600 body-font ">
+        <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link href='/'>
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">  
-                <img src="/Group.svg"/>
+                <img src="/logo-landscape.png"/>
             </a>
           </Link> 
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
@@ -22,7 +22,7 @@ const NavBar = ({cartQty}) => {
                   <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                 </svg>
                 <span className="absolute inset-0 object-right-top -mr-6">
-                  <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-indigo-500 text-white">
+                  <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-blue-500 text-white">
                     {cartQty}
                   </div>
                 </span>

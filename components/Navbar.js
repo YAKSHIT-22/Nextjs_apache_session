becd8e4c984202bf338f2f73d90e12f6c8e0e353
Yakshit <92084737+YAKSHIT-22@users.noreply.github.com>
2022-12-31 21:22:46 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,7 @@ const NavBar = ({cartQty}) => {
             <Link href='/about'><a className="mr-5 hover:text-gray-900">About</a></Link>
             <Link href='/products'><a className="mr-5 hover:text-gray-900">Products</a></Link>
             <Link href='/contact'><a className="mr-5 hover:text-gray-900">Contact Us</a></Link>
-            <Link href='/cart'>
+          
               <button className="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
                 <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" stroke="currentColor">
                   <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
@@ -27,7 +27,7 @@ const NavBar = ({cartQty}) => {
                   </div>
                 </span>
               </button>
-            </Link>
+            
           </nav>
           {/* <button className="my-2 text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded text-sm">Login</button> */}
         </div>
